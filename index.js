35caf95193f4f6817e35455e5c54285564f47e2e
Saifur Rahman Mohsin <mohsin92@me.com>
2021-07-28 17:01:52 +0530

    Update

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-const core = require('@actions/core');
+const core  = require('@actions/core');
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
+  const instagramId = core.getInput('instagram-id');
+  const instagramAccessToken = core.getInput('instagram-access-token');
+  const URL = 'https://graph.instagram.com/v11.0/' + instagramId + '/media?access_token=' + instagramAccessToken;
+  fetch(URL,  init:{method: 'GET', headers:{}})
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
 } catch (error) {
